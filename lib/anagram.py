51c1dfb9d40a0d8e7dffa688ad3f6348f7e0b8b1
Dean14692-prog <dennismuasyangui@gmail.com>
2025-05-14 00:21:38 +0300

    commit

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,25 @@
-# your code goes here!
+
+
+class Anagram:
+    
+    def __init__(self,word = 'listen'):
+        self.word = word
+        pass
+
+    def match(self, list):
+        sorted_word = sorted(self.word)
+
+        matches = []
+
+        for word in list:
+
+            if sorted(word) == sorted_word:
+                matches.append(word)
+
+        return matches
+
+        
+
+        pass
+
+word = Anagram('enlist')
